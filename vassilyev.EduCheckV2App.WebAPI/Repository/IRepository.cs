55a847de5f797d57b09857fceeb49f3e8a2324a4
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-07-17 16:59:59 +0500

    Create base interface for repository

---
+++ vassilyev.EduCheckV2App.WebAPI/Repository/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using vassilyev.EduCheckV2App.WebAPI.Entities;
+
+namespace vassilyev.EduCheckV2App.WebAPI.Repository;
+
+public interface IRepository<T> where T: class
+{
+    Task<ICollection<T>> GetAllAsync();
+    Task<T> GetAsync(Guid id);
+    Task CreateAsync(T item);
+    void Update(T item);
+    void Remove(T item);
+    Task SaveAsync();
+}
