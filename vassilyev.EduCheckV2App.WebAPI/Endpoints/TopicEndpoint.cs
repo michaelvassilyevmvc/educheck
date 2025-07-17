db0c92e9a0b486b5aa960b84a05444e7dca9004b
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-07-17 17:01:51 +0500

    Implement Topic Repo and Endpoint

---
+++ vassilyev.EduCheckV2App.WebAPI/Endpoints/TopicEndpoint.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using Carter;
+using vassilyev.EduCheckV2App.WebAPI.Entities;
+using vassilyev.EduCheckV2App.WebAPI.Helpers;
+using vassilyev.EduCheckV2App.WebAPI.Repository;
+
+namespace vassilyev.EduCheckV2App.WebAPI.Endpoints;
+
+public class TopicEndpoint : CarterModule
+{
+    public TopicEndpoint() : base("api/topics")
+    {
+    }
+
+    public override void AddRoutes(IEndpointRouteBuilder app)
+    {
+        // GET
+        app.MapGet("/", GetAll)
+            .WithName("GetTopics")
+            .Produces<Topic>(201);
+    }
+
+    private static async Task<IResult> GetAll(IRepository<Topic> _repo, ILogger<Program> _logger)
+    {
+        APIResponse response = new();
+        _logger?.LogInformation("Get all topics");
+        response.IsSuccess = true;
+        response.Result = await _repo.GetAllAsync();
+        response.StatusCode = HttpStatusCode.OK;
+        return Results.Ok(response);
+    }
+}
---
+++ vassilyev.EduCheckV2App.WebAPI/Repository/TopicRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using vassilyev.EduCheckV2App.WebAPI.Data;
+using vassilyev.EduCheckV2App.WebAPI.Entities;
+
+namespace vassilyev.EduCheckV2App.WebAPI.Repository;
+
+public class TopicRepository: IRepository<Topic>
+{
+    private readonly ApplicationDbContext _db;
+
+    public TopicRepository(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+    
+    public async Task<ICollection<Topic>> GetAllAsync()
+    {
+        return await _db.Topics.AsNoTracking().ToListAsync();
+    }
+
+    public async Task<Topic> GetAsync(Guid id)
+    {
+        return await _db.Topics.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+    }
+
+    public async Task<Topic> GetAsync(string topicText)
+    {
+        return await _db.Topics.AsNoTracking().FirstOrDefaultAsync(x => x.Name.ToLower().StartsWith(topicText.ToLower()));
+    }
+
+    public async Task CreateAsync(Topic topic)
+    {
+        await _db.Topics.AddAsync(topic);
+    }
+
+    public void Update(Topic topic)
+    {
+        _db.Topics.Update(topic);
+    }
+
+    public void Remove(Topic topic)
+    {
+        _db.Topics.Remove(topic);
+    }
+
+    public async Task SaveAsync()
+    {
+        await _db.SaveChangesAsync();
+    }
+}
