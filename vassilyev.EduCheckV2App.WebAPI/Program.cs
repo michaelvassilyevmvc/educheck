7820de00df35ed4bb9e0f995ae391c96dffbc57e
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-07-17 14:12:25 +0500

    Add Db Context to middleware conveyar

--- vassilyev.EduCheckV2App.WebAPI/Program.cs
+++ vassilyev.EduCheckV2App.WebAPI/Program.cs
@@ -1,6 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using vassilyev.EduCheckV2App.WebAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
