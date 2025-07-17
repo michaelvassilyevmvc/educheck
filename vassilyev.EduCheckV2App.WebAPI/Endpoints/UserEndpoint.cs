c0bcc929fb24f43ac1eb0a70ec9fdbfb7ad9f06d
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-07-17 17:01:34 +0500

    Implement User Repo and Endpoint

---
+++ vassilyev.EduCheckV2App.WebAPI/Endpoints/UserEndpoint.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using Carter;
+using vassilyev.EduCheckV2App.WebAPI.Entities;
+using vassilyev.EduCheckV2App.WebAPI.Helpers;
+using vassilyev.EduCheckV2App.WebAPI.Repository;
+
+namespace vassilyev.EduCheckV2App.WebAPI.Endpoints;
+
+public class UserEndpoint : CarterModule
+{
+    public UserEndpoint() : base("api/users")
+    {
+    }
+
+    public override void AddRoutes(IEndpointRouteBuilder app)
+    {
+        // GET
+        app.MapGet("/", GetAll)
+            .WithName("GetUsers")
+            .Produces<User>(201);
+    }
+
+    private static async Task<IResult> GetAll(IRepository<User> _repo, ILogger<Program> _logger)
+    {
+        APIResponse response = new();
+        _logger?.LogInformation("Get all users");
+        response.IsSuccess = true;
+        response.Result = await _repo.GetAllAsync();
+        response.StatusCode = HttpStatusCode.OK;
+        return Results.Ok(response);
+    }
+}
---
+++ vassilyev.EduCheckV2App.WebAPI/Repository/UserRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using vassilyev.EduCheckV2App.WebAPI.Data;
+using vassilyev.EduCheckV2App.WebAPI.Entities;
+
+namespace vassilyev.EduCheckV2App.WebAPI.Repository;
+
+public class UserRepository: IRepository<User>
+{
+    private readonly ApplicationDbContext _db;
+
+    public UserRepository(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+    
+    public async Task<ICollection<User>> GetAllAsync()
+    {
+        return await _db.Users.AsNoTracking().ToListAsync();
+    }
+
+    public async Task<User> GetAsync(Guid id) => await _db.Users.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+
+    public async Task<User> GetAsync(string userName)
+    {
+        return await _db.Users.AsNoTracking().FirstOrDefaultAsync(x => x.Login.ToLower() == userName.ToLower());
+    }
+
+    public async Task CreateAsync(User user)
+    {
+        await _db.Users.AddAsync(user);
+    }
+
+    public void Update(User user)
+    {
+        _db.Users.Update(user);
+    }
+
+    public void Remove(User user)
+    {
+        _db.Users.Remove(user);
+    }
+
+    public async Task SaveAsync()
+    {
+        await _db.SaveChangesAsync();
+    }
+}
