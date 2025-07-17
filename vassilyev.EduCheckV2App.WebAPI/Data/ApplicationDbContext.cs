8a67cfa50cb81e01d6d1778576ac52746b203e79
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-07-17 14:00:11 +0500

    Add DB context with all entities

---
+++ vassilyev.EduCheckV2App.WebAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using vassilyev.EduCheckV2App.WebAPI.Entities;
+
+namespace vassilyev.EduCheckV2App.WebAPI.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Question> Questions { get; set; }
+    public DbSet<Topic> Topics { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserSession> UserSessions { get; set; }
+    public DbSet<AnswerOption> AnswerOptions { get; set; }
+    public DbSet<SessionStats> SessionStats { get; set; }
+}
