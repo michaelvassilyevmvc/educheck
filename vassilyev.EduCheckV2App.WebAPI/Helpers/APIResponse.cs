5cfb5bd7e1baea7fc73e349461e152a540cd6fb6
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-07-17 16:59:14 +0500

    Create response standart answer type

---
+++ vassilyev.EduCheckV2App.WebAPI/Helpers/APIResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+namespace vassilyev.EduCheckV2App.WebAPI.Helpers;
+
+public class APIResponse
+{
+    public bool IsSuccess { get; set; }
+    public Object Result { get; set; }
+    public HttpStatusCode StatusCode { get; set; }
+    public List<string> ErrorMessages { get; set; } = new List<string>();
+}
